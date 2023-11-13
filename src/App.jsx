90cc2ec22d5717bed35709159c5c0973c937d7f9
abhi goyal <abhigoyal72621289@gmail.com>
2023-11-13 15:48:29 +0530

    product filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 
 import './App.css'
 import Navbar from './components/NavBar/navbar'
-import MainCarousel from './components/HomeCarousel/MainCarousel'
-import ProductCarosel from './components/HomeSectionCard/ProductCarosel'
+// import MainCarousel from './components/HomeCarousel/MainCarousel'
+// import ProductCarosel from './components/HomeSectionCard/ProductCarosel'
 import Footer from './components/Footer/Footer'
+import Product from './components/Products/Product'
 function App() {
   
 
   return (
     <>
      <Navbar/>
-     <MainCarousel/>
-     <ProductCarosel/>
+     {/* <MainCarousel/>
+     <ProductCarosel/> */}
+     <Product/>
+
      <Footer/>
     
     </>
